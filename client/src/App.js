2e9a191c1617cf98aaced6372bd64c954d427184
r0mka <romanliveshere@gmail.com>
2021-03-03 20:59:31 -0800

    Prepare for deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { msg: null };
+  }
+  componentDidMount() {
+    axios
+      .get('/api')
+      .then((res) => this.setState({ msg: res.data.msg }));
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <h1>{this.state.msg ? this.state.msg : ''}</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
